719e76d83101f6e5b771db2c490eb0bd5eb386de
shri-ram8 <shriram2310624@ssn.edu.in>
2026-02-21 15:11:51 +0530

    Removed PhonePe service registration

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<SmartSlot.Services.DistanceService>();
-builder.Services.AddScoped<SmartSlot.Services.PhonePeService>();
+
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
