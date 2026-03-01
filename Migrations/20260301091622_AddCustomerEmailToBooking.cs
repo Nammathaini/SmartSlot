4c5f3b863e15c9af7fadfd466191b683d9bdf4b3
shri-ram8 <shriram2310624@ssn.edu.in>
2026-03-01 14:46:45 +0530

    Add migration for CustomerEmail

---
+++ Migrations/20260301091622_AddCustomerEmailToBooking.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartSlot.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerEmailToBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerEmail",
+                table: "Bookings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerEmail",
+                table: "Bookings");
+        }
+    }
+}
